ce5ad1227c7491ce8a985c57a44ab0ce2a153ae5
OseeMukaya <oseemukaya@gmail.com>
2020-07-04 23:59:21 +0200

    tout va bien

--- routers/EmployeRouter.js
+++ routers/EmployeRouter.js
@@ -3,8 +3,8 @@ const Router = express.Router();
 const EmployeController = require('../controllers/EmployeController');
 
 Router.post("/api/employes", EmployeController.createEmploye());
-Router.delete('/api/employes', EmployeController.deleteEmploye());
-Router.put('/api/empoyes', EmployeController.updateEmploye());
+Router.post('/api/employes', EmployeController.deleteEmploye());
+Router.post('/api/empoyes', EmployeController.updateEmploye());
 Router.get('/api/employes', EmployeController.getEmploye());
 
 module.exports = Router;
