98bfbd8e914fdc1c64260ff9aedbfbad28875d62
OseeMukaya <oseemukaya@gmail.com>
2020-07-05 11:21:52 +0200

    correction

--- routers/EmployeRouter.js
+++ routers/EmployeRouter.js
@@ -4,7 +4,7 @@ const EmployeController = require('../controllers/EmployeController');
 
 Router.post("/api/employes", EmployeController.createEmploye());
 Router.post('/api/employes/delete', EmployeController.deleteEmploye());
-Router.post('/api/empoyes/edit', EmployeController.updateEmploye());
+Router.post('/api/employes/edit', EmployeController.updateEmploye());
 Router.get('/api/employes', EmployeController.getEmploye());
 
 module.exports = Router;
